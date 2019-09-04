4df1a9f14927a299762e1d678c1f095b54c824b9
J Guillermo <jguillermo@lacafetalab.pe>
2019-09-04 17:20:12 -0500

    chore unit test

--- container/dev/resources/test.sh
+++ container/dev/resources/test.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-nosetests --with-xunit -v /application/tests/unit/suite_test.py
+# nosetests --with-xunit -v /application/tests/unit/suite_test.py
+# --cover-package=src
+
+nosetests --cover-branches --with-coverage --cover-erase --cover-html --cover-package=src --with-xunit -v /application/tests/unit/suite_test.py
